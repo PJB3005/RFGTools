a61bf4d2ac926da2581f1e5c1d18ef75c9962f61
moneyl <8206401+Moneyl@users.noreply.github.com>
2019-03-30 21:17:04 -0400

    Initial vpp/str2 unpacker done
    Needs testing, so next I'm going to write a tool that can scan all files in a folder, and check for any corrupted rfg files. Will be useful for other file format tools too

--- RFGTools/Program.cs
+++ RFGTools/Program.cs
@@ -1,15 +1,42 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
 
-namespace RFGTools
+namespace RFGFormats
 {
     class Program
     {
         static void Main(string[] args)
         {
+            /*
+            Args:
+            Default: PackfileUnpacker.exe InputFile [OutputFolder] ... Auto unpacks to folder with the same name if not OutputFolder provided, maybe strip off extension for less confusion
+            -a: Unpack all in folder, args: PackfileUnpacker.exe InputFolder [OutputFolder] ... If no OutputFolder provided, unpack to ./unpack/packfilename, perhaps with extension stripped
+            Consider adding option to auto detect wanted behavior depending on if a folder or file is dropped on it.
+            Have messagebox pop up to confirm action on activities which might take forever.
+            */
+
+
+            /*
+            if(args.Length > 1)
+            {
+
+            }
+            string PackfilePath = @"B:\RFG Unpack\Unpack\unpack1\vehicles_r\civ_truck1.ccar_pc.str2_pc";//@"B:\RFG Unpack\data\misc.vpp_pc";
+            string OutputPath = @"B:\RFG Unpack\Unpack\unpack1\vehicles_r\civ_truck1_alt\";
+            var Packfile = new Packfile3();
+            Packfile.Deserialize(PackfilePath, OutputPath);*/
+
+            FileInfo[] DataFolder = new DirectoryInfo(@"B:\RFG Unpack\data\").GetFiles();
+            foreach(var File in DataFolder)
+            {
+                var Packfile = new Packfile3();
+                string PackfilePath = File.FullName;
+                string OutputFolderPath = File.Directory.Parent.FullName + @"\Unpack\unpack2\" + File.Name + @"\";
+                Packfile.Deserialize(PackfilePath, OutputFolderPath);
+            }
+            Console.WriteLine("Complete. Press any key to close.");
+            Console.ReadKey(); //Waits for keypress to exit.
         }
     }
 }
