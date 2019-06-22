7bee87293f917e5424396de13a49a02253d04b25
moneyl <8206401+Moneyl@users.noreply.github.com>
2019-06-22 15:48:49 -0400

    Initial Packfile Packer (WIP)

---
+++ PackfilePacker/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using RFGTools.Formats.Packfile3;
+
+namespace PackfilePacker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputPath = @"C:\Users\moneyl\RFG Unpack\Unpack\unpack3\steam.vpp_pc\";
+            string outputPath = @"C:\Users\moneyl\RFG Unpack\Pack\steam.vpp_pc";
+
+            var Packfile = new Packfile3(true);
+            Packfile.Serialize(inputPath, outputPath, true, false);
+
+            Console.WriteLine("\nComplete! Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
