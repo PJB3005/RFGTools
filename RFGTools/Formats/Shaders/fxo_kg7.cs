4f3a1a3ca02547584d493cf276b9a64f6d6595d6
moneyl <8206401+Moneyl@users.noreply.github.com>
2019-08-02 16:18:51 -0400

    Add shader extraction tool for rfg (.fxo_kg files)
    Mostly ignores the rfg specific header at the beginning of the file, finds all the dx11 shaders inside the fxo_kg file, and extracts them, and disassembles them using a library called SlimShader. A lot of room for improvement here such as allowing editing and repacking, or writing a parser that makes the disassembly look more like real code.

---
+++ RFGTools/Formats/Shaders/fxo_kg7.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SlimShader;
+
+namespace RFGTools.Formats.Shaders
+{
+    class fxo_kg7
+    {
+        //rfgr data -- Data prepended to the actual shader which is used by rfg
+        uint Signature;
+        uint Version;
+        uint ShaderFlags;
+        ushort NumTexFixups; //Unknown usage //ushort + 2 bytes null. 
+        ushort NumConstFixups; //Unknown usage
+        ushort NumBoolFixups; //Unknown usage
+        char NumVertexShaders;
+        char NumPixelShaders;
+        int vertex_shaders; //offset
+        int pixel_shaders; //offset
+        int tex_fixups; //offset
+        int const_fixups; //offset
+        int bool_fixups; //offset
+        char BaseTechniqueIndex;
+        char LightingTechniqueIndex;
+        char DepthOnlyTechniqueIndex;
+        char UserTechniqueStartIndex;
+        int ShaderTechniques; //offset
+        uint NumTechniques;
+
+
+
+        //D3D11 data -- Data from the actual shader that's loaded by D3D11
+        //
+
+        void Deserialize(string inputPath, string outputPath)
+        {
+            
+        }
+    }
+}
+
+//Useful structs dumped from rfg.pdb
+
+//struct rl_shader_header
+//{
+//    int signature;
+//    int version;
+//    unsigned int shader_flags;
+//    __int16 num_tex_fixups;
+//    __int16 num_const_fixups;
+//    __int16 num_bool_fixups;
+//    char num_vertex_shaders;
+//    char num_pixel_shaders;
+//    et_ptr_offset<rl_vertex_shader,0> vertex_shaders; //Note: All these et_ptr_offset values are just ints
+//    et_ptr_offset<rl_pixel_shader,0> pixel_shaders;
+//    et_ptr_offset<rl_shader_tex_fixup,0> tex_fixups;
+//    et_ptr_offset<rl_shader_const_fixup,0> const_fixups;
+//    et_ptr_offset<rl_shader_const_fixup,0> bool_fixups;
+//    char base_technique_index;
+//    char lighting_technique_index;
+//    char depth_only_technique_index;
+//    char user_technique_start_index;
+//    et_ptr_offset<rl_shader_technique,0> shader_techniques;
+//    int num_techniques;
+//};
+
+//struct rl_shader_header_runtime
+//{
+//    int signature;
+//    int version;
+//    unsigned int shader_flags;
+//    __int16 num_tex_fixups;
+//    __int16 num_const_fixups;
+//    __int16 num_bool_fixups;
+//    char num_vertex_shaders;
+//    char num_pixel_shaders;
+//    rl_dev_vertex_shader* vertex_shaders;
+//    rl_dev_pixel_shader* pixel_shaders;
+//    rl_shader_tex_fixup_runtime* tex_fixups;
+//    rl_shader_const_fixup* const_fixups;
+//    rl_shader_const_fixup* bool_fixups;
+//    char base_technique_index;
+//    char lighting_technique_index;
+//    char depth_only_technique_index;
+//    char user_technique_start_index;
+//    rl_shader_technique_runtime* shader_techniques;
+//    int num_techniques;
+//};
---
+++ ShaderTool/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommandLine;
+
+namespace ShaderTool
+{
+    class Program
+    {
+        public class Options
+        {
+            [Value(0, MetaName = "Path", Required = true, HelpText = "Input file path.")]
+            public string Path { get; set; }
+
+            [Option('c', "Compiled", Required = false, HelpText = "Dump compiled shaders for use with tools like SlimShader Studio.")]
+            public bool DumpCompiledShaders { get; set; }
+        }
+
+        static void Main(string[] args)
+        {
+            Parser.Default.ParseArguments<Options>(args).WithParsed(options =>
+            {
+                ExtractRfgDx11Shader(options.Path, options.DumpCompiledShaders);
+            });
+#if DEBUG
+            Console.WriteLine("\nComplete! Press any key to exit.");
+            Console.ReadKey();
+#endif
+        }
+
+        public static void ExtractRfgDx11Shader(string shaderPath, bool dumpCompiledShaders)
+        {
+            string shaderName = Path.GetFileNameWithoutExtension(shaderPath);
+            if (!File.Exists(shaderPath))
+            {
+                throw new Exception($"\"{shaderPath}\" is not a valid file path!");
+            }
+
+            using (var stream = new BinaryReader(new FileStream(shaderPath, FileMode.Open)))
+            {
+                uint signature = stream.ReadUInt32();
+                if (signature != 1262658030)
+                {
+                    throw new Exception($"Invalid file signature! Expected 1262658030, found {signature}. Check that the vpp_pc file containing the shader was extracted properly.");
+                }
+
+                uint version = stream.ReadUInt32();
+                if (version != 7)
+                {
+                    if (version == 9)
+                    {
+                        throw new Exception($"Unsupported shader format version. Found version 9. This shader file is likely from RFA due to the version.");
+                    }
+                    else
+                    {
+                        throw new Exception($"Unsupported shader format version! Expected version 7, found version {version}.");
+                    }
+                }
+
+                stream.ReadBytes(10); //Skip 10 bytes since those values don't matter for the moment. Ignoring the RFG header info for now.
+                uint numVertexShaders = stream.ReadByte();
+                uint numPixelShaders = stream.ReadByte();
+                uint totalShaderCount = numVertexShaders + numPixelShaders;
+
+                Directory.CreateDirectory("./ShaderDump/");
+                for (int i = 0; i < totalShaderCount; i++)
+                {
+                    SeekToNextDx11Shader(stream);
+#if DEBUG
+                    Console.WriteLine($"Found dx11 shader header at byte offset {stream.BaseStream.Position}");
+#endif
+                    stream.ReadBytes(24); //Skip unneeded values
+                    int shaderSize = stream.ReadInt32();
+                    stream.BaseStream.Seek(-28, SeekOrigin.Current); //Return to shader header
+
+                    var shaderBuffer = stream.ReadBytes(shaderSize);
+
+                    if (dumpCompiledShaders)
+                    {
+                        File.WriteAllBytes($"./ShaderDump/{shaderName}_compiled{i}.dx11", shaderBuffer);
+                    }
+
+                    var bytecodeContainer = new SlimShader.BytecodeContainer(shaderBuffer);
+                    File.WriteAllText($"./ShaderDump/{shaderName}_disassembled{i}.txt", bytecodeContainer.ToString());
+                }
+            }
+        }
+
+        public static void SeekToNextDx11Shader(BinaryReader stream)
+        {
+            while (true)
+            {
+                if (stream.ReadByte() == 'D')
+                {
+                    if (stream.ReadByte() == 'X')
+                    {
+                        if (stream.ReadByte() == 'B')
+                        {
+                            if (stream.ReadByte() == 'C')
+                            {
+                                stream.BaseStream.Seek(-4, SeekOrigin.Current);
+                                return;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
